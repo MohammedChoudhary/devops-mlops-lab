bda426ab41afca4cc0c60abe0b6c5eb1ad030bf4
mohammad7-dev <mohdchoudhary007@gmail.com>
2026-02-12 23:42:42 +0530

    Added app.py pipeline script

---
+++ app.py
@@ -0,0 +1,7 @@
+print("DevOps + MLOps Lab Pipeline Started")
+
+print("Step 1: Code is running successfully")
+
+print("Step 2: This script will later be automated using GitHub Actions")
+
+print("Pipeline finished without errors")
